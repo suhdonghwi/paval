05ad2e9ee5cf627dfa784a426033c06d0864fd5c
suhdonghwi <hwidongsuh@gmail.com>
2020-09-12 19:15:50 +0900

    feat: add simple text echo feature

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use tbot::prelude::*;
+
 #[tokio::main]
 async fn main() {
-    println!("hello, world!");
+    let mut bot = tbot::from_env!("TBOT_TOKEN").event_loop();
+
+    bot.text(|context| async move {
+        let echo = &context.text.value;
+        let call_result = context.send_message(echo).call().await;
+
+        if let Err(err) = call_result {
+            dbg!(err);
+        }
+    });
+
+    bot.polling().start().await.unwrap();
 }
